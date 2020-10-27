41bb4e5c47790b26b05e0a41bad09faeae739714
dannymckinney88 <dannymckinney88@gmail.com>
2020-10-27 12:36:13 -0700

    Pulled in changes for favorties added delet. Also got reviews to save to db

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -10,9 +10,7 @@ router.use(methodOverride('_method'))
 //Display a list of the users favorites 
 router.get('/', (req, res) =>{
     db.favorite.findAll().then(favorites => {
-        console.log('-------------------------------------------------------------------------------------')
         console.log(favorites[0].dataValues)
-        console.log('-------------------------------------------------------------------------------------')
         res.render('movies/favorites',{favoriteMovies: favorites})
 
     })
@@ -26,13 +24,8 @@ router.post('/', (req,res) =>{
             movieId: req.body.movieId
         }
     }).then(createFavorite => {
-        //console.log(createFavorite)
-        // redirect 
         res.redirect('/favorites')
     })
-    //console.log(req.user)
-    //console.log(req.body)
-   // console.log(req.body.movieId)
 })
 
 // Delete a movie from the users favorites list
